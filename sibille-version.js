fcbd53b1b0c5a96724dc2e502ca7f13e6fa5705d
sibille79 <sibille.etling@gmail.com>
2021-10-19 00:04:16 +0200

    add frame

--- sibille-version.js
+++ sibille-version.js
@@ -1,11 +1,18 @@
 var xBall = Math.floor(Math.random() * 300) + 50;
 var yBall = Math.floor(Math.random() * 500) + 50;
-var xSpeed = (1, 4);
-var ySpeed = (-7, -2);
+var xSpeed = (4, 8);
+var ySpeed = (-8, -4);
 var score = 0
-var ballSize = 200
+var ballSize = 100
 var radius = ballSize/2
 var backgrColour
+var newSize
+//var windowHeight = 500
+//var windowWidth = 1000
+var rectWidth = 150
+var rectHeight = 20
+var fieldWidth = 1250
+var fieldHeight = 650
 
 /* object-orientierte Schreibweise
 let ball = {
@@ -20,6 +27,7 @@ let ball = {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
   backgrColour = color(0, 0, 0)
 }
 
@@ -30,9 +38,22 @@ function changeColour() {
     backgrColour = color(newR, newG, newB)
 }
 
+function changeSize(){
+    newW = (random(10, 100))
+    newH = (random(10, 100))
+newSize = ellipse(xBall, yBall, newS, newS)
+}
+
 function draw() {
   // Background
   background(backgrColour);
+  translate(50, 50);
+  //Linie
+  line(50, 50, 1250, 50);
+  line(50, 50, 50, 650);
+  line(1250, 50, 1250, 650);
+  line(50, 650, 1250, 650);
+  
   
   // Score
   textSize(24);
@@ -47,9 +68,9 @@ function draw() {
   
   if(mouseIsPressed) {
     fill(color(255, 0, 255));
-    ellipse(mouseX,windowHeight-15, 150, 15);
+    ellipse(mouseX,fieldHeight-15, 150, 15);
   } else {
-    rect(mouseX,windowHeight-15, 90, 15);
+    rect(mouseX,fieldHeight-15, rectWidth, rectHeight);
   }
  
   // TO DO 2: Schaffst du es, dass sich der Ball frei bewegt?
@@ -58,7 +79,7 @@ function draw() {
   ellipse(xBall, yBall, ballSize, ballSize);
 
   // TO DO 3: Lass den Ball von den Seitenrändern abprallen (windowWidth/windowHeight)
-  if(xBall>windowWidth-radius || xBall<0+radius){
+  if(xBall>fieldWidth-radius || xBall+radius<0){
     xSpeed *=-1;
   } 
   
@@ -67,18 +88,18 @@ function draw() {
   }
 
 // Hintergrundfarbe ändert sich, sobald der Ball am Rand anstösst
-  if(xBall>windowWidth-radius|| xBall+radius<0) {
+  if(xBall>fieldWidth-radius || xBall<0 || yBall<0) {
     changeColour()
     }
 
   // TO DO 4: Lass den Ball vom Balken aprallen, falls sie sich berühren
-
-  if((yBall+radius>windowHeight-15) && (xBall>mouseX && xBall<mouseX+90)){
-     /* andere Schreibweise
-     xSpeed = -xSpeed;
-     */
+  if((yBall+radius>fieldHeight-rectHeight) && (xBall>mouseX && xBall<mouseX+rectWidth)){
     xSpeed *=-1;
     ySpeed *=-1; 
+    /* andere Schreibweise
+     xSpeed = -xSpeed;
+     */
+    score++;
     }
   }
   
