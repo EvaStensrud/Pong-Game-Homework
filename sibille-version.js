721836347b814605f808d7f591a65aaa20b78cda
sibille79 <sibille.etling@gmail.com>
2021-10-20 00:33:58 +0200

    add Game over function

--- sibille-version.js
+++ sibille-version.js
@@ -6,12 +6,10 @@ var score = 0
 var ballSize = 100
 var radius = ballSize/2
 var backgrColour
-var newSize
-//var windowHeight = 500
-//var windowWidth = 1000
+//var colourBall
 var rectWidth = 150
 var rectHeight = 20
-var fieldWidth = 1250
+var fieldWidth = 1000
 var fieldHeight = 650
 
 /* object-orientierte Schreibweise
@@ -29,8 +27,9 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   backgrColour = color(0, 0, 0)
+  //colourBall = fillStyle(0 ,0 , 0)
 }
-
+//Hintergrundfarbe ändert sich (Bedingung: bei Seitenkontakt wird unten aufgerufen)
 function changeColour() {
     var newR = random(255)
     var newG = random(255)
@@ -38,61 +37,80 @@ function changeColour() {
     backgrColour = color(newR, newG, newB)
 }
 
-function changeSize(){
-    newW = (random(10, 100))
-    newH = (random(10, 100))
-newSize = ellipse(xBall, yBall, newS, newS)
+/*//Farbe Ball ändert sich random
+function changeColourBall() {
+    var ballR = random(255)
+    var ballG = random(255)
+    var ballB = random(255)
+    colourBall = fillStyle(ballR, ballG, ballB)
+}*/
+
+//Game over
+function gameOver() {
+    text("Game over", 450, 350);  
 }
 
 function draw() {
-  // Background
+// Background
   background(backgrColour);
+
+//Ballfarbe
+    //fillStyle(colourBall);
+
+// Koordinatensystem verschieben
   translate(50, 50);
-  //Linie
-  line(50, 50, 1250, 50);
-  line(50, 50, 50, 650);
-  line(1250, 50, 1250, 650);
-  line(50, 650, 1250, 650);
+
+//Linie oben
+  line(0, 0, 1000, 0);
+//Linie links
+  line(0, 0, 0, 650);
+//Linie rechts
+  line(1000, 0, 1000, 650);
+
+  stroke('purple'); // Change the color
+  strokeWeight(10);
   
-  
-  // Score
+// Score
   textSize(24);
-  text("Score: " + score, 10, 25);
+  text("Score: " + score, 1050, 10);
+  text("Bei grün: abprallen lassen", 1050, 50);
+  text("Bei rot: durch lassen", 1050, 90);
+
 
 /*if(yBall>=windowHeight){
     score +=1
    }
 */
 
-  // TO DO 1: Bringe den Balken dazu der Maus auf der x-Achse zu folgen. 
+// TO DO 1: Bringe den Balken dazu der Maus auf der x-Achse zu folgen. 
   
-  if(mouseIsPressed) {
+  /*if(mouseIsPressed) {
     fill(color(255, 0, 255));
-    ellipse(mouseX,fieldHeight-15, 150, 15);
-  } else {
-    rect(mouseX,fieldHeight-15, rectWidth, rectHeight);
-  }
+    ellipse(mouseX,fieldHeight-rectHeight, rectWidth, rectHeight);
+  } else {*/
+    rect(mouseX,fieldHeight-rectHeight, rectWidth, rectHeight);
+  //}
  
-  // TO DO 2: Schaffst du es, dass sich der Ball frei bewegt?
+// TO DO 2: Schaffst du es, dass sich der Ball frei bewegt?
   xBall += xSpeed;
   yBall += ySpeed;
   ellipse(xBall, yBall, ballSize, ballSize);
 
-  // TO DO 3: Lass den Ball von den Seitenrändern abprallen (windowWidth/windowHeight)
-  if(xBall>fieldWidth-radius || xBall+radius<0){
+// TO DO 3: Lass den Ball von den Seitenrändern abprallen (windowWidth/windowHeight)
+  if(xBall>fieldWidth-radius || xBall+radius<100){
     xSpeed *=-1;
   } 
-  
+
   if(yBall<0+radius){
     ySpeed *=-1
   }
 
 // Hintergrundfarbe ändert sich, sobald der Ball am Rand anstösst
-  if(xBall>fieldWidth-radius || xBall<0 || yBall<0) {
+  if(xBall>fieldWidth-radius || xBall+radius<100 || yBall<0+radius) {
     changeColour()
     }
 
-  // TO DO 4: Lass den Ball vom Balken aprallen, falls sie sich berühren
+// TO DO 4: Lass den Ball vom Balken aprallen, falls sie sich berühren
   if((yBall+radius>fieldHeight-rectHeight) && (xBall>mouseX && xBall<mouseX+rectWidth)){
     xSpeed *=-1;
     ySpeed *=-1; 
@@ -100,6 +118,12 @@ function draw() {
      xSpeed = -xSpeed;
      */
     score++;
+   // changeColourBall();
     }
+
+    if (yBall>fieldHeight) {
+    gameOver()
+    }
+
   }
   
